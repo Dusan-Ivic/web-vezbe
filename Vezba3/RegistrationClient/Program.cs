53891316f2623deeb936a3693197412524cf0d45
Dusan-Ivic <ivicd99@gmail.com>
2022-03-26 13:15:24 +0100

    Vezba 3

---
+++ Vezba3/RegistrationClient/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationClient
+{
+	class Program
+	{
+		public static void StartClient()
+		{
+			//Connect to a remote device.
+			try
+			{
+				//Establish the remote endpoint for the socket.
+				//This example uses port 11000 on the local computer.
+				//IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");//Dns.Resolve(Dns.GetHostName())
+
+				//IPAddress ipAddress = ipHostInfo.AddressList[0];
+				IPAddress ipAddress = IPAddress.Loopback;
+				IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+				//Create a TCP/IP socket.
+				Socket socket = new Socket(
+					AddressFamily.InterNetwork,
+					SocketType.Stream,
+					ProtocolType.Tcp
+				);
+
+				//Connect the socket to the remote endpoint. Catch any errors.
+				try
+				{
+					socket.Connect(remoteEP);
+
+					Console.WriteLine("Socket connected to {0}", socket.RemoteEndPoint.ToString());
+
+					NetworkStream stream = new NetworkStream(socket);
+					StreamReader sr = new StreamReader(stream);
+					StreamWriter sw = new StreamWriter(stream)
+					{
+						NewLine = "\r\n",
+						AutoFlush = true
+					};
+
+					Console.WriteLine("(EXIT / LIST / ADD username,... / REMOVE username / FIND username)");
+					while (true)
+					{
+						Console.Write(">> ");
+						string command = Console.ReadLine();
+
+						if (command.ToUpper().StartsWith("EXIT"))
+						{
+							sw.WriteLine(command);
+							break;
+						}
+						else if (command.ToUpper().StartsWith("ADD"))
+						{
+							sw.WriteLine(command);
+							while (true)
+							{
+								string response = sr.ReadLine();
+								if (response == "END")
+								{
+									break;
+								}
+								Console.WriteLine(response);
+							}
+						}
+						else if (command.ToUpper().StartsWith("LIST"))
+						{
+							sw.WriteLine(command);
+							while (true)
+							{
+								string response = sr.ReadLine();
+								if (response == "END")
+								{
+									break;
+								}
+								Console.WriteLine(response);
+							}
+						}
+						else if (command.ToUpper().StartsWith("REMOVE"))
+						{
+							sw.WriteLine(command);
+							Console.WriteLine(sr.ReadLine());
+						}
+						else if (command.ToUpper().StartsWith("FIND"))
+						{
+							sw.WriteLine(command);
+							Console.WriteLine(sr.ReadLine());
+						}
+					}
+
+					//Release the socket.
+					sr.Close();
+					sw.Close();
+					stream.Close();
+					socket.Shutdown(SocketShutdown.Both);
+					socket.Close();
+				}
+				catch (ArgumentNullException ane)
+				{
+					Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+				}
+				catch (SocketException se)
+				{
+					Console.WriteLine("SocketException : {0}", se.ToString());
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine("Unexpected exception : {0}", e.ToString());
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			StartClient();
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Vezba3/RegistrationServer/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationServer
+{
+	class Program
+	{
+		public static ArrayList users = new ArrayList();
+
+		public static void StartListening()
+		{
+			//Establish the local endpoint for the socket.
+			//Dns.GetHostName returns the name of the host running the application.
+			//IPHostEntry ipHostInfo = Dns.GetHostEntry("192.168.1.208");//Dns.Resolve(Dns.GetHostName());
+			//IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+
+			//IPAddress ipAddress = ipHostInfo.AddressList[8];
+			IPAddress ipAddress = IPAddress.Loopback; //ipHostInfo.AddressList[0];
+			IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+			//Create a TCP/IP socket.
+			Socket serverSocket = new Socket(
+				ipAddress.AddressFamily,//AddressFamily.InterNetwork
+				SocketType.Stream,
+				ProtocolType.Tcp
+			);
+
+			// Bind the socket to the local endpoint and listen for incoming connections.
+			try
+			{
+				serverSocket.Bind(localEndPoint);
+				serverSocket.Listen(10);
+
+				// Start listening for connections.
+				while (true)
+				{
+					Console.WriteLine("Waiting for a connection...");
+					// Program is suspended while waiting for an incoming connection.
+					Socket socket = serverSocket.Accept();
+
+					Task<int> t = Task.Factory.StartNew(() => Run(socket));
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			Console.WriteLine("\nPress ENTER to continue...");
+			Console.ReadLine();
+		}
+
+		private static int Run(Socket socket)
+		{
+			NetworkStream stream = new NetworkStream(socket);
+			StreamReader sr = new StreamReader(stream);
+			StreamWriter sw = new StreamWriter(stream)
+			{
+				NewLine = "\r\n",
+				AutoFlush = true
+			};
+
+			while (true)
+			{
+				Console.WriteLine("Receiving data...");
+				string line = sr.ReadLine();
+				Console.WriteLine("Stiglo od klijenta: {0}", line);
+
+				if (line.ToUpper().StartsWith("EXIT"))
+				{
+					break;
+				}
+				else if (line.ToUpper().StartsWith("ADD"))
+				{
+					string[] usernames = line.Substring(line.IndexOf(' ') + 1).Split(',');
+					foreach (string username in usernames)
+					{
+						if (username.Trim().Length > 0)
+						{
+							if (!users.Contains(username.Trim()))
+							{
+								users.Add(username.Trim());
+								sw.WriteLine("KORISNIK '{0}' JE USPESNO REGISTROVAN!", username.Trim());
+							}
+							else
+							{
+								sw.WriteLine("KORISNIK '{0}' VEC POSTOJI!", username.Trim());
+							}
+						}
+					}
+					sw.WriteLine("END");
+				}
+				else if (line.ToUpper().StartsWith("LIST"))
+				{
+					foreach (string user in users)
+					{
+						sw.WriteLine(user);
+					}
+					sw.WriteLine("END");
+				}
+				else if (line.ToUpper().StartsWith("REMOVE"))
+				{
+					string username = line.Split(' ')[1].Trim();
+					if (users.Contains(username))
+					{
+						users.Remove(username);
+						sw.WriteLine("KORISNIK '{0}' JE OBRISAN!", username);
+					}
+					else
+					{
+						sw.WriteLine("KORISNIK '{0}' NE POSTOJI!", username);
+					}
+				}
+				else if (line.ToUpper().StartsWith("FIND"))
+				{
+					string username = line.Split(' ')[1].Trim();
+					if (users.Contains(username))
+					{
+						sw.WriteLine("KORISNIK '{0}' JE REGISTROVAN!", username);
+					}
+					else
+					{
+						sw.WriteLine("KORISNIK '{0}' NE POSTOJI!", username);
+					}
+				}
+			}
+
+			sr.Close();
+			sw.Close();
+			stream.Close();
+
+			socket.Shutdown(SocketShutdown.Both);
+			socket.Close();
+			return 0;
+		}
+
+		static void Main(string[] args)
+		{
+			StartListening();
+		}
+	}
+}
